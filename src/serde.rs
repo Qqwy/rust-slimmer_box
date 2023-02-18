cfb1a722f8027401c59cbd0b6694da47e93e96ba
qqwy <qqwy@gmx.com>
2023-02-19 00:40:55 +0100

    Tests for serde support

--- src/serde.rs
+++ src/serde.rs
@@ -1,15 +1,30 @@
 use alloc::boxed::Box;
 use alloc::vec::Vec;
+use alloc::string::String;
 
 use ptr_meta::Pointee;
-use ::serde::ser::Serialize;
+use ::serde::ser::{Serialize, Serializer};
 use ::serde::de::{Deserialize, Deserializer};
 
 use crate::{SlimmerBox, SlimmerPointee};
 
-impl<'de, T, SlimmerMetadata,> Deserialize<'de> for SlimmerBox<T, SlimmerMetadata>
+impl<T: Serialize, SlimmerMetadata> Serialize for SlimmerBox<T, SlimmerMetadata>
 where
-    T: Sized,
+    T: ?Sized,
+    T: SlimmerPointee<SlimmerMetadata>,
+    SlimmerMetadata: TryFrom<<T as Pointee>::Metadata> + TryInto<<T as Pointee>::Metadata> + Copy,
+{
+
+fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    (**self).serialize(serializer)
+}
+}
+
+impl<'de, T: Sized + Clone, SlimmerMetadata,> Deserialize<'de> for SlimmerBox<T, SlimmerMetadata>
+where
     T: SlimmerPointee<SlimmerMetadata>,
     SlimmerMetadata: TryFrom<<T as Pointee>::Metadata> + TryInto<<T as Pointee>::Metadata> + Copy,
     T: Deserialize<'de>,
@@ -46,7 +61,7 @@ where
     where
         D: Deserializer<'de>,
     {
-        Deserialize::deserialize(deserializer).map(|val| SlimmerBox::from_box(Vec::into_boxed_slice(val)))
+        Deserialize::deserialize(deserializer).map(|val| SlimmerBox::from_box(String::into_boxed_str(val)))
     }
 }
 
@@ -56,8 +71,31 @@ mod tests {
     use serde_test::{Token, assert_tokens};
 
     #[test]
-    fn serde_round_trip() {
+    fn serde_round_trip_sized() {
+        let boxed: SlimmerBox<u64, _> = SlimmerBox::new(&64);
+        assert_tokens(&boxed, &[
+            Token::U64(64),
+        ]);
+    }
+
+    #[test]
+    fn serde_round_trip_slice() {
         let boxed: SlimmerBox<[i32]> = SlimmerBox::new([1, 2, 3, 4].as_slice());
-        assert_tokens(&boxed, []);
+        assert_tokens(&boxed, &[
+            Token::Seq { len: Some(4)},
+            Token::I32(1),
+            Token::I32(2),
+            Token::I32(3),
+            Token::I32(4),
+            Token::SeqEnd,
+        ]);
+    }
+
+    #[test]
+    fn serde_round_trip_str() {
+        let boxed: SlimmerBox<str> = SlimmerBox::new("hello");
+        assert_tokens(&boxed, &[
+            Token::Str("hello"),
+        ]);
     }
 }
